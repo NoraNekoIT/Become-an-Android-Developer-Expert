a79a0e998b1c6a33539bdb930c6d2b01993db5ba
NoraNekoIT <bunsmart18@gmail.com>
2022-04-01 11:41:10 +0700

    update

--- core/src/main/java/com/noranekoit/made/core/utils/AppExecutors.kt
+++ core/src/main/java/com/noranekoit/made/core/utils/AppExecutors.kt
@@ -1,5 +1,7 @@
 package com.noranekoit.made.core.utils
 
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.VisibleForTesting
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
@@ -7,11 +9,19 @@ import java.util.concurrent.Executors
 class AppExecutors @VisibleForTesting constructor(
     private val diskIO: Executor
 ) {
+    companion object {
+        private const val THREAD_COUNT = 3
+    }
     constructor() : this(
         Executors.newSingleThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
 
-
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
 }
