e584857764508c4b9ce75dc90e2ab46398cc2c45
NoraNekoIT <bunsmart18@gmail.com>
2022-04-18 12:53:50 +0700

    update encryption,obfuscation, certificate pinning

--- app/src/main/java/com/noranekoit/made/submission/MainActivity.kt
+++ app/src/main/java/com/noranekoit/made/submission/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import com.noranekoit.made.submission.databinding.ActivityMainBinding
@@ -15,7 +16,6 @@ import com.noranekoit.made.submission.home.HomeFragment
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -67,5 +67,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        binding.navView.menu[0].isChecked =true
     }
 }
--- core/src/main/java/com/noranekoit/made/core/di/CoreModule.kt
+++ core/src/main/java/com/noranekoit/made/core/di/CoreModule.kt
@@ -9,6 +9,9 @@ import com.noranekoit.made.core.data.source.remote.RemoteDataSource
 import com.noranekoit.made.core.data.source.remote.network.ApiService
 import com.noranekoit.made.core.domain.repository.IMovieRepository
 import com.noranekoit.made.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -17,32 +20,43 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-
 val databaseModule = module{
     factory{
         get<MovieDatabase>().movieDao()
     }
     single {
+        val passphrase:ByteArray = SQLiteDatabase.getBytes("noranekoit".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java,"Movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname,"sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .add(hostname,"sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname,"sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         if (BuildConfig.DEBUG) {
             OkHttpClient.Builder()
                 .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
+                .certificatePinner(certificatePinner)
                 .build()
         }
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
