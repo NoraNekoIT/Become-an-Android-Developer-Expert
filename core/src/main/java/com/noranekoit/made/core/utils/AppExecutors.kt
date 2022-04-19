25dac3c989246dcf2e7d8c5290915a0f8516e33e
NoraNekoIT <bunsmart18@gmail.com>
2022-04-19 11:27:56 +0700

    update

--- core/src/main/java/com/noranekoit/made/core/utils/AppExecutors.kt
+++ core/src/main/java/com/noranekoit/made/core/utils/AppExecutors.kt
@@ -1,23 +1,21 @@
 package com.noranekoit.made.core.utils
 
-import android.os.Handler
-import android.os.Looper
 import androidx.annotation.VisibleForTesting
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
 class AppExecutors @VisibleForTesting constructor(
     private val diskIO: Executor,
-    private val networkIO: Executor,
-    private val mainThread: Executor
+//    private val networkIO: Executor,
+//    private val mainThread: Executor
 ) {
-    companion object {
-        private const val THREAD_COUNT = 3
-    }
+//    companion object {
+//        private const val THREAD_COUNT = 3
+//    }
     constructor() : this(
         Executors.newSingleThreadExecutor(),
-        Executors.newFixedThreadPool(THREAD_COUNT),
-        MainThreadExecutor()
+//        Executors.newFixedThreadPool(THREAD_COUNT),
+//        MainThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
@@ -25,10 +23,10 @@ class AppExecutors @VisibleForTesting constructor(
 //    fun networkIO(): Executor = networkIO
 //
 //    fun mainThread(): Executor = mainThread
-    private class MainThreadExecutor : Executor {
-        private val mainThreadHandler = Handler(Looper.getMainLooper())
-        override fun execute(command: Runnable) {
-            mainThreadHandler.post(command)
-        }
-    }
+//    private class MainThreadExecutor : Executor {
+//        private val mainThreadHandler = Handler(Looper.getMainLooper())
+//        override fun execute(command: Runnable) {
+//            mainThreadHandler.post(command)
+//        }
+//    }
 }
